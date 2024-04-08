823553afdb6a733e2a73f8dc281f675a4bc71927
mayur2905 <mayurgaikwad0101@gmail.com>
2024-04-09 00:20:12 +0530

    Update EKS cluster role and AWS IAM role for EKS node group in main.tf

--- TF-jenkins-Docker-K8/TF-EKS/main.tf
+++ TF-jenkins-Docker-K8/TF-EKS/main.tf
@@ -10,22 +10,17 @@ data "aws_subnets" "this_subnet"{
 }
 # Create an EKS cluster role
 resource "aws_iam_role" "example" {
-  name = "eks-node-group-role"
-  assume_role_policy = jsondecode(
-    {
-      Statement = [
-        {
-          Effect = "Allow",
-          Principal = {
-            Service = "eks.amazonaws.com"
-          },
-          Action = "sts:AssumeRole"
-        }
-      ]
-      Version = "2012-10-17"
-    }
-  )
-  
+  name = "eks-node-group-example"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
 }
 # Create an EKS cluster. 
 resource "aws_eks_cluster" "this_eks" {
